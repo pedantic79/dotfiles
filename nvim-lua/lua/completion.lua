cda790058b605d2d67b5c4576b81d2012b977f63
Dan Nguyen <pedantic@gmail.com>
2022-03-12 09:04:26 -0600

    chore(nvim): add start of lua based neovim config

---
+++ nvim-lua/lua/completion.lua
@@ -0,0 +1,70 @@
+local cmp_status_ok, cmp = pcall(require, "cmp")
+if not cmp_status_ok then
+  return
+end
+
+local lspkind_status_ok, lspkind = pcall(require, "lspkind")
+if not lspkind_status_ok then
+  return
+end
+
+cmp.setup {
+  mapping = {
+    ["<C-k>"] = cmp.mapping.select_prev_item(),
+		["<C-j>"] = cmp.mapping.select_next_item(),
+    ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+    ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+    ["<C-e>"] = cmp.mapping {
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    },
+    -- Accept currently selected item. If none selected, `select` first item.
+    -- Set `select` to `false` to only confirm explicitly selected items.
+    ["<CR>"] = cmp.mapping.confirm { select = true },
+    ["<Tab>"] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback()
+      end
+    end,
+    ["<S-Tab>"] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      else
+        fallback()
+      end
+    end
+  },
+  sources = {
+    { name = "path" },
+    { name = "nvim_lua" },
+    { name = "nvim_lsp" },
+    { name = "buffer", keyword_length = 5 }
+  },
+  snippets = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body)
+    end,
+  },
+
+  formatting = {
+    format = lspkind.cmp_format {
+      with_text = true,
+      menu = {
+        buffer = "[buf]",
+        nvim_lsp = "[LSP]",
+        nvim_lua = "[api]",
+        path = "[path]",
+        luasnip = "[snip]",
+      }
+    }
+  },
+
+  experimental = {
+    native_menu = false,
+    ghost_text = true,
+  }
+}
---
+++ nvim-lua/lua/plugins.lua
@@ -0,0 +1,70 @@
+local fn = vim.fn
+
+local install_path = fn.stdpath("data") .. "/site/pack/packer/opt/packer.nvim"
+
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  local packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup({function(use)
+  -- My plugins here
+  use { 'wbthomason/packer.nvim', opt = true }
+
+  -- Theme
+  use 'dracula/vim'
+  use 'morhetz/gruvbox'
+  use 'vim-airline/vim-airline'
+  use 'vim-airline/vim-airline-themes'
+  use 'airblade/vim-gitgutter'
+
+  -- GUI Enhancement
+  use 'machakann/vim-highlightedyank'
+  -- use 'nvim-lua/plenary.nvim'
+  -- use 'nvim-lua/popup.nvim'
+
+  -- Icons
+  use 'ryanoasis/vim-devicons'
+
+  -- LSP
+  use {
+    'neovim/nvim-lspconfig',
+    'williamboman/nvim-lsp-installer',
+  }
+  use 'simrat39/rust-tools.nvim'
+  use 'onsails/lspkind-nvim'
+
+  -- Completion
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-nvim-lua'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use "saadparwaiz1/cmp_luasnip"
+
+  -- Treesitter
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+  }
+
+  -- Languages
+  -- use 'crispgm/nvim-go'
+  use 'ray-x/go.nvim'
+
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end,
+config = {
+  display = {
+    open_fn = function()
+      return require('packer.util').float({ border = 'single' })
+    end
+  }
+}})
