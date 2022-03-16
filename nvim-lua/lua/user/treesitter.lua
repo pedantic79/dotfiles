fdfaeed9dac44d33f8ac3f0f5d87c47bd9ec803e
Dan Nguyen <pedantic@gmail.com>
2022-03-16 14:42:47 -0500

    chore(nvim): update tree-sitter list

--- nvim-lua/lua/user/treesitter.lua
+++ nvim-lua/lua/user/treesitter.lua
@@ -3,8 +3,36 @@ if not status_ok then
   return
 end
 
+local languages = {
+  "bash",
+  "css",
+  "go",
+  "dockerfile",
+  "go",
+  "gomod",
+  "graphql",
+  "java",
+  "javascript",
+  "jsdoc",
+  "json",
+  "json5",
+  "kotlin",
+  "make",
+  "norg",
+  "perl",
+  "python",
+  "regex",
+  "ruby",
+  "rust",
+  "scss",
+  "toml",
+  "typescript",
+  "vim",
+  "yaml"
+}
+
 configs.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = languages, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "" }, -- List of parsers to ignore installing
   autopairs = {
