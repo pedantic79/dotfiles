c2fc489d15e302b507f7495f5d9909281b475726
Dan Nguyen <pedantic@gmail.com>
2022-03-28 19:58:18 -0500

    refactor(nvim): move nvim-lua-install.sh

--- bin/nvim-lua-install.sh
+++ bin/nvim-lua-install.sh
@@ -4,4 +4,4 @@ rm -rf .local/share/nvim/ .cache/nvim .config/nvim/plugin
 
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' && \
     nvim --headless -c "TSInstallSync maintained" -c "quitall" && \
-    nvim --headless  -c "sleep 60" -c "quitall"
+    nvim --headless -c "sleep 60" -c "quitall"
