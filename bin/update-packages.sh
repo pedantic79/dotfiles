28ba6af40234063c9837a20cf1c361ed3132cb9a
Dan Nguyen <pedantic@gmail.com>
2022-05-10 11:45:06 -0500

    Update .dotfiles and .omz_custom

--- bin/update-packages.sh
+++ bin/update-packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$(uname -s)" = "Darwin" ]; then
-    brew upgrade && brew cleanup -s --prune 0 && rustup update
+    brew upgrade && brew cleanup -s --prune 0 && rustup update && (cd ~/.dotfiles && git up) && (cd ~/.omz_custom && git up)
 else
     echo "ERROR unsupport $(uname -s)"
 fi
