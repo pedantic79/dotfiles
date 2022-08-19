b0ff4b6de41aceeaed6ce358edb200e5ca90c993
Dan Nguyen <pedantic@gmail.com>
2022-08-18 20:28:21 -0500

    Manaully install Tree-sitter parsers

--- bin/nvim-lua-install.sh
+++ bin/nvim-lua-install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-rm -rf .local/share/nvim/ .cache/nvim .config/nvim/plugin
+TS_LIST="javascript c cpp dockerfile rust lua python go gomod graphql bash "
+TS_LIST+="fish php kotlin html julia json css scss c_sharp todotxt typescript "
+TS_LIST+="tsx scala haskell toml yaml regex comment jsdoc zig vim help json5 "
+TS_LIST+="llvm make norg"
 
+rm -rf "$HOME/.local/share/nvim/" "$HOME/.cache/nvim" "$HOME/.config/nvim/plugin"
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' && \
-    nvim --headless -c "TSInstallSync all" -c "quitall" && \
+    nvim --headless -c "TSInstallSync $TS_LIST" -c "quitall" && \
     nvim --headless -c "sleep 90" -c "quitall"
 
 date
