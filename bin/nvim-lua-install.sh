804e7ab4881f68402f0b086ee2392f8eb066c3f0
Dan Nguyen <pedantic@gmail.com>
2022-10-03 13:20:37 -0500

    chore(bin): update install of treesitter

--- bin/nvim-lua-install.sh
+++ bin/nvim-lua-install.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-TS_LIST="javascript c cpp dockerfile rust lua python go gomod graphql bash "
-TS_LIST+="fish php kotlin html julia json css scss c_sharp todotxt typescript "
-TS_LIST+="tsx scala haskell toml yaml regex comment jsdoc zig vim help json5 "
-TS_LIST+="llvm make norg"
-
 rm -rf "$HOME/.local/share/nvim/" "$HOME/.cache/nvim" "$HOME/.config/nvim/plugin"
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' && \
-    nvim --headless -c "TSInstallSync $TS_LIST" -c "sleep 30" -c "quitall" && \
     nvim --headless -c "sleep 30" -c "quitall"
 
 echo ""
--- nvim-lua/lua/user/treesitter.lua
+++ nvim-lua/lua/user/treesitter.lua
@@ -3,10 +3,22 @@ if not status_ok then
   return
 end
 
+local ignore_list = { 'agda', 'astro', 'beancount', 'bibtex', 'blueprint',
+  'commonlistp', 'cooklang', 'cuda', 'd', 'dart', 'devicetree', 'eex', 'elixir',
+  'elm', 'elvish', 'embedded_template', 'erlang', 'fennel', 'foam', 'fortran',
+  'fusion', 'gdscript', 'gleam', 'glimmer', 'glsl', 'godot_resource', 'hack',
+  'hcl', 'heex', 'hjson', 'hlsl', 'hocon', 'julia', 'jsonnet', 'latex',
+  'lalrpop', 'ledger', 'm68k', 'menhir', 'meson', 'ninja', 'nix', 'pascal',
+  'phpdoc', 'pioasm', 'prisma', 'proto', 'pug', 'ql', 'qmljs', 'query', 'r',
+  'racket', 'rasi', 'rego', 'rnoweb', 'rst', 'scala', 'scheme', 'slint',
+  'solidity', 'sparql', 'sql', 'supercollider', 'surface', 'svelte', 'sxhkdrc',
+  'teal', 'tiger', 'tlaplus', 'todotxt', 'turtle', 'twig', 'v', 'vala',
+  'verilog', 'vue', 'wgsl', 'yang' }
+
 configs.setup {
-  -- ensure_installed = languages, -- one of 'all', 'maintained' (parsers with maintainers), or a list of languages
-  -- sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-  ignore_install = { '' }, -- List of parsers to ignore installing
+  ensure_installed = 'all', -- one of 'all', 'maintained' (parsers with maintainers), or a list of languages
+  sync_install = true, -- install languages synchronously (only applied to `ensure_installed`)
+  ignore_install = ignore_list, -- List of parsers to ignore installing
   autopairs = {
     enable = true,
   },
