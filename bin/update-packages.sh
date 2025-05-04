4464a38c5b2927a6899da9fbde4a410f92466d06
Dan Nguyen <pedantic@gmail.com>
2025-05-03 21:54:15 -0500

    Update update-packages.sh

--- bin/update-packages.sh
+++ bin/update-packages.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 if [ "$(uname -s)" = "Darwin" ]; then
-    brew upgrade && brew cleanup -s --prune 0 && rustup update && (cd ~/.dotfiles && git up) && (cd ~/.omz_custom && git up)
+    brew upgrade &&
+        brew cleanup -s --prune 0 &&
+        rustup update &&
+        (cd ~/.dotfiles && git up) &&
+        (cd ~/.omz_custom && git up) &&
+        ([ -e "$HOME/.cargo/bin/cargo-install-update" ] &&
+            cargo install-update --all)
 else
     echo "ERROR unsupport $(uname -s)"
 fi
