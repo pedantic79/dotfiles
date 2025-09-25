a3463e910890c9bdf97bbe54fc5be495f870d07c
Dan Nguyen <pedantic@gmail.com>
2025-09-25 10:13:40 -0500

    Pin catppuccin version to fix issue

--- lazyvim/lua/plugins/colorscheme.lua
+++ lazyvim/lua/plugins/colorscheme.lua
@@ -6,4 +6,9 @@ return {
       colorscheme = "catppuccin",
     },
   },
+  -- the following fixes a bug. it should be deleted eventually
+  {
+    "catppuccin/nvim",
+    commit = "f19cab18ec4dc86d415512c7a572863b2adbcc18",
+  },
 }
