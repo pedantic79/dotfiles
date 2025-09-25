eb7b2854750d0fe7ddfde456de07c8cce30e3057
Dan Nguyen <pedantic@gmail.com>
2025-09-25 10:07:26 -0500

    revert: work around catppuccin issue
    This reverts commit e0132df912550c46431b0b89d0f50ceca71f8112.

--- lazyvim/lua/plugins/colorscheme.lua
+++ lazyvim/lua/plugins/colorscheme.lua
@@ -6,15 +6,4 @@ return {
       colorscheme = "catppuccin",
     },
   },
-  -- the following fixes a bug. it should be deleted eventually
-  {
-    "catppuccin/nvim",
-    opts = function(_, opts)
-      local module = require("catppuccin.groups.integrations.bufferline")
-      if module then
-        module.get = module.get_theme
-      end
-      return opts
-    end,
-  },
 }
