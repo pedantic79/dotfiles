74258f07877140786d69b7ccc5d17f4e6dcc4fad
Dan Nguyen <pedantic@gmail.com>
2025-09-26 08:48:06 -0500

    Revert "Pin catppuccin version to fix issue"
    This reverts commit a3463e910890c9bdf97bbe54fc5be495f870d07c.

--- lazyvim/lua/plugins/colorscheme.lua
+++ lazyvim/lua/plugins/colorscheme.lua
@@ -6,9 +6,4 @@ return {
       colorscheme = "catppuccin",
     },
   },
-  -- the following fixes a bug. it should be deleted eventually
-  {
-    "catppuccin/nvim",
-    commit = "f19cab18ec4dc86d415512c7a572863b2adbcc18",
-  },
 }
