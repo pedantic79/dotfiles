b961c1b93d1c314f2a2674f730ce7bdfa8004134
Dan Nguyen <pedantic@gmail.com>
2025-10-06 10:32:56 -0500

    refactor(packages): refactor update-packages.sh

--- bin/update-packages.sh
+++ bin/update-packages.sh
@@ -3,9 +3,9 @@
 if [ "$(uname -s)" = "Darwin" ]; then
     brew upgrade &&
         brew cleanup -s --prune 0 &&
-        rustup update &&
         (cd ~/.dotfiles && git up) &&
         (cd ~/.omz_custom && git up) &&
+        rustup update &&
         ([ -e "$HOME/.cargo/bin/cargo-install-update" ] &&
             cargo install-update --all)
 else
