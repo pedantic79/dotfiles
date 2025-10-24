870d39f0d25c437eda4a06c5be21339bddf3add3
Dan Nguyen <pedantic@gmail.com>
2025-10-23 22:17:48 -0500

    chore(packages): add Debian/Linux to update-packages.sh

--- bin/update-packages.sh
+++ bin/update-packages.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-if [ "$(uname -s)" = "Darwin" ]; then
+OS=$(uname -s)
+if [ "$OS" = "Darwin" ]; then
     brew upgrade &&
         brew cleanup -s --prune 0 &&
         (cd ~/.dotfiles && git up) &&
@@ -8,6 +9,16 @@ if [ "$(uname -s)" = "Darwin" ]; then
         rustup update &&
         ([ -e "$HOME/.cargo/bin/cargo-install-update" ] &&
             cargo install-update --all)
+elif [ "$OS" = "Linux" ]; then
+    if [ -e "/etc/debian_version" ]; then
+        sudo apt -y upgrade && sudo apt -y upgrade
+    fi
+
+    if [ -x "$HOME/.appimage/appimageupdatetool-$(uname -m).AppImage" ]; then
+        for i in "$HOME/.appimage/"*.[Aa]pp[Ii]mage; do
+            "$HOME/.appimage/appimageupdatetool-$(uname -m).AppImage" "$i"
+        done
+    fi
 else
     echo "ERROR unsupport $(uname -s)"
 fi
