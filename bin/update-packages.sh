6a7ec2c22295611ff1911d0981da21a5048ff243
Dan Nguyen <pedantic@gmail.com>
2025-10-23 22:58:14 -0500

    chore(packages): more updates to update-packages.sh

--- bin/update-packages.sh
+++ bin/update-packages.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
+set -e
+
 OS=$(uname -s)
 ARCH=$(uname -m)
 if [ "$OS" = "Darwin" ]; then
-    brew upgrade &&
-        brew cleanup -s --prune 0 &&
-        (cd ~/.dotfiles && git up) &&
-        (cd ~/.omz_custom && git up) &&
-        rustup update &&
-        ([ -e "$HOME/.cargo/bin/cargo-install-update" ] &&
-            cargo install-update --all)
+    brew upgrade
+    brew cleanup -s --prune 0
+
+    (cd ~/.dotfiles && git up)
+    (cd ~/.omz_custom && git up)
+
+    if [ -x "$HOME/.cargo/bin/rustup" ]; then
+        rustup update "$(rustup toolchain list | awk '/default/ {print $1}')"
+    fi
+
+    if [ -e "$HOME/.cargo/bin/cargo-install-update" ]; then
+        cargo install-update --all
+    fi
 elif [ "$OS" = "Linux" ]; then
     if [ -e "/etc/debian_version" ]; then
         sudo apt -y upgrade && sudo apt -y upgrade
     fi
+
+    (cd ~/.dotfiles && git up)
+    (cd ~/.omz_custom && git up)
 
     if [ -x "$HOME/.appimage/appimageupdatetool-$ARCH.AppImage" ]; then
         for i in "$HOME/.appimage/"*.[Aa]pp[Ii]mage; do
@@ -22,6 +33,7 @@ elif [ "$OS" = "Linux" ]; then
     fi
 else
     echo "ERROR unsupport $OS"
+    exit 1
 fi
 
 if [ -n "$ZSH" ] && [ -d "$ZSH" ]; then
