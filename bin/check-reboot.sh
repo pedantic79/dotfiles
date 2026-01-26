5ccf814704493d78ec5901148a189b12a69a47c7
Dan Nguyen <pedantic@gmail.com>
2026-01-26 12:16:56 -0600

    Only send email once for check-reboot.sh

--- bin/check-reboot.sh
+++ bin/check-reboot.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+U="${EMAIL:-$USER}"
+reboot_sent="/tmp/.${U}-check-reboot/sent"
+
 if [ -f /var/run/reboot-required ]; then
-    echo "Reboot required on $(hostname) due to unattended-upgrades." | mail -s "Reboot Required: $(hostname)" "${EMAIL:-$USER}"
+    if [ ! -f "$reboot_sent" ]; then
+        echo "Reboot required on $(hostname) due to unattended-upgrades." | mail -s "Reboot Required: $(hostname)" "$U"
+        mkdir -p $(dirname "$reboot_sent")
+        touch "$reboot_sent"
+    fi
 fi
